fe17193433c065fef057900a30601c4996090913
Дарья Юдакова <facemag1234@gmail.com>
2023-10-06 20:44:38 +0300

    Add foreign languages

--- hello.pas
+++ hello.pas
@@ -1,3 +1,8 @@
 ﻿begin
-  
+  var n := ReadInteger('Введите язык: 1) Английский 2) Русский 3) Немецкий');
+  case n of
+    1: Println('Hello');
+    2: Println('Привет');
+    3: Println('Hallo');
+  end;
 end.
